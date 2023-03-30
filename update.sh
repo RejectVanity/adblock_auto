d47e890ffcb5576631622032723736df1cbe6599
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2023-03-30 13:44:33 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -49,7 +49,7 @@ cp -rf "${Base_Rules_Folder}/adblock_lite" "${Combine_Folder}/lite/adblock_lite.
 cp -rf "${Base_Rules_Folder}/其他.prop" "${Combine_Folder}/lite/其他.txt"
 Combine_adblock_original_file "${Rules_Folder}/adblock_auto_lite.txt" "${Combine_Folder}/lite"
 modtify_adblock_original_file "${Rules_Folder}/adblock_auto_lite.txt"
-make_white_rules "${Rules_Folder}/adblock_auto_lite.txt"
+#make_white_rules "${Rules_Folder}/adblock_auto_lite.txt"
 write_head "${Rules_Folder}/adblock_auto_lite.txt" "混合规则精简版(自动更新)" && echo "※`date +'%F %T'` 混合规则精简版合并完成！"
 
 #rm -rf "`pwd`/temple"
