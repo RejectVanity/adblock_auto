aff23dc2e8dc9296a4ea70481ac248148d25478d
lingeringsound <114198798+lingeringsound@users.noreply.github.com>
2023-08-16 12:47:02 +0800

    Update until_function.sh

--- until_function.sh
+++ until_function.sh
@@ -43,8 +43,10 @@ cat << key > "${file}"
 ! Homepage: https://lingeringsound.github.io/adblock_auto
 ! Gitcode Homepage: https://gitcode.net/weixin_45617236/adblock_auto
 ! GitHub Homepage: https://github.com/lingeringsound/adblock_auto
+! GITlab Homepage: https://jihulab.com/foreseeable_boy/adblock_auto
 ! Gitcode Raw Link: https://gitcode.net/weixin_45617236/adblock_auto/-/raw/main/Rules/${file##*/}
 ! Github Raw Link: https://lingeringsound.github.io/adblock_auto/Rules/${file##*/}
+! GITlab Raw Link: https://jihulab.com/foreseeable_boy/adblock_auto/-/raw/main/Rules/${file##*/}
 key
 echo "${original_file}" >> "${file}"
 busybox sed -i 's/换行符正则表达式n/\\/g' "${file}"
